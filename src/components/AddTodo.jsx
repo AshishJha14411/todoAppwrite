094f9a5b15c603b10ff99861e3946a6c41b293bf
Ashish Jha <ashishjha14411@gmail.com>
2022-12-02 21:04:53 +0530

    added add todo component

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
-
+import {v4 as uuidv4} from 'uuid'
+import { databases } from '../appwrite/appwriteConfig'
 const AddTodo = () => {
+
+  const [todos, setTodo] = userState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const promise = databases.createDocument("6389e334af481ba13d9c", uuidv4(), {
+      todos
+    })
+    promise.then(
+      function(res){
+        console.log(res)
+      },
+      function(error){
+        console.log(error)
+      }
+    )
+  }
   return (
     <div>AddTodo</div>
   )
